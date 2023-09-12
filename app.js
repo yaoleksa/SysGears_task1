3c6b5e7200fded2e7728cf4f2e410498ed43bf34
yaoleksa <mryaremchyk@gmail.com>
2023-09-12 18:09:32 +0300

    extend backend

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+const parse = require('http-body-json-parse');
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.end("Hello world!");
+    if(req.method == 'POST') {
+        parse(req, 500).then(data => {
+            const s = '' + data.num;
+            res.end(JSON.stringify(s));
+        });
+    }
+    fs.readFile('index.html', function(error, data){
+        if(error){
+            res.writeHead(404)
+            res.write('Error: File not found')
+        }else{
+            res.write(data)
+        }
+        res.end()
+    });
 });
 server.listen(3000, () => {
     console.log(`http://localhost:${3000}`);
